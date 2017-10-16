390934617706fb0838a4122d51884b17568be707
MurphyKyle <m.kyle1@yahoo.com>
2017-10-16 09:59:15 -0600

    Peer coded the first grid into it's working state. Need to create the second grid "overlay".

--- Pente/Pente/MainWindow.xaml.cs
+++ Pente/Pente/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,57 @@ namespace Pente
         public MainWindow()
         {
             InitializeComponent();
+			CreateGrid(); 
         }
-    }
+
+		public void CreateGrid()
+		{
+			mainGrid.Columns = 19;
+			mainGrid.Rows = 19;
+
+			for (int row = 0; row < 19; row++)
+			{
+				//grid1.ColumnDefinitions.Add(new ColumnDefinition());
+
+				for (int col = 0; col < 19; col++)
+				{
+					//grid1.RowDefinitions.Add(new RowDefinition());
+
+					Label label = new Label() {
+						BorderBrush = Brushes.DarkGray,
+						BorderThickness = new Thickness(1.0),
+						Background = Brushes.Green,
+						Width = 30,
+						Height = 30
+					};
+
+					label.PreviewMouseDown += ProcessLabel_Click;
+
+					mainGrid.Width = (label.Width) * 19;
+					mainGrid.Height = (label.Height) * 19;
+					mainWindow.Width = label.Width * 19.5;
+					mainWindow.Height = label.Height * 20.3;
+
+					mainGrid.Children.Add(label);					
+				}
+			}
+
+			Border border = new Border();
+			border.Width = mainGrid.Width;
+			border.Height = mainGrid.Height;
+		}
+
+		private void ProcessLabel_Click(object sender, RoutedEventArgs e)
+		{
+			Label label = (Label)sender;
+
+			if (label.Background == Brushes.Green)
+			{
+				label.Background = Brushes.Azure;
+				return;
+			}
+
+			label.Background = Brushes.Green;
+		}
+	}
 }
