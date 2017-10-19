f954dc7dd3724828b83a1e94eabba4a53311059e
MurphyKyle <m.kyle1@yahoo.com>
2017-10-19 08:33:11 -0600

    Adding UnitTest Project and source

---
+++ Pente/PenteUnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Pente;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Controls.Primitives;
+
+namespace PenteUnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		private MainWindow main = new MainWindow();
+
+		[TestMethod]
+		public void GridCreationTest()
+		{
+			UniformGrid grid = main.GetMainGrid;
+			int rows = grid.Rows;
+			int cols = grid.Columns;
+			int expected = rows * cols;
+			int childCount = grid.Children.Count;
+
+			Assert.AreEqual(expected, childCount, "Amount of children did not match what was expected!");
+			
+		}
+	}
+}
