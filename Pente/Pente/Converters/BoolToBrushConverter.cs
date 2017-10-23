e0ee87fed6eb357308d6be6ccc824e34a0fafc58
MurphyKyle <m.kyle1@yahoo.com>
2017-10-23 00:50:55 -0600

    Converters that got left behind

---
+++ Pente/Pente/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Pente.Converters
+{	
+	[Serializable]
+	public class BoolToBrushConverter : IValueConverter
+	{
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool? boolVal = (bool?)value;
+			Ellipse e = (Ellipse)parameter;
+			Brush b;
+
+			if (boolVal == null)
+			{
+				b = Brushes.Transparent;
+			}
+			else if (boolVal == true)
+			{
+				b = Brushes.White;
+			}
+			else // boolVal == false
+			{
+				b = Brushes.Black;
+			}
+
+			e.Fill = b;
+			return b;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Pente/Pente/Converters/BoolToOpacityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pente.Converters
+{
+	[Serializable]
+	public class BoolToOpacityConverter : IValueConverter
+	{
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool? boolVal = (bool?)value;
+			double opacity = 0.2;
+			
+			if (boolVal != null)
+			{
+				opacity = 100.0;
+			}
+
+			return opacity;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
